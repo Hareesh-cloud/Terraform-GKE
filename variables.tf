ef64520ec22424be91da844819ec0aaf79d8d10b
Hareesh5041 <82581268+Hareesh5041@users.noreply.github.com>
2021-06-14 12:35:12 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,52 @@
+variable "project_id" {
+  type        = string
+  description = "Project Identifier"
+  default     = "decent-habitat-315907"
+}
+
+variable "project_name" {
+  type        = string
+  description = "Cluster Name"
+  default     = "gcp cluster"
+}
+
+variable "project_zone" {
+  type        = string
+  description = "Google Computer zone to use for the cluster"
+  default     = "us-central1-a"
+}
+
+variable "project_region" {
+  type        = string
+  description = "Project Region"
+  default     = "us-west2" # Los Angeles, CA, USA
+}
+
+variable "project_prefix" {
+  type        = string
+  description = "Project Prefix"
+  default     = "gke-demo"
+}
+
+variable "node_pools_oauth_scopes" {
+  type        = list
+  description = "oAuth Scopes for Node Pools"
+  default = [
+    "https://www.googleapis.com/auth/devstorage.read_only",
+    "https://www.googleapis.com/auth/logging.write",
+    "https://www.googleapis.com/auth/monitoring",
+    "https://www.googleapis.com/auth/servicecontrol",
+    "https://www.googleapis.com/auth/service.management.readonly",
+    "https://www.googleapis.com/auth/trace.append",
+  ]
+}
+
+locals {
+  subnet_name = "${var.project_prefix}-subnet"
+}
+
+# get your public IP address by querying ICHI
+data "http" "icanhazip" {
+  # this value will be available in data.http.icanhazip.body
+  url = "https://icanhazip.com/"
+}
